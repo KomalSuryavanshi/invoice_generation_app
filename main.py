a4f45b3f141fb167a8dbadb9b97177f04aa7d5d2
Komal <komalwork11@gmail.com>
2023-04-02 00:27:15 +0530

    Enter the elements in the cell

--- main.py
+++ main.py
@@ -7,13 +7,41 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")  # Use glob to read all xlsx files
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_no = filename.split("-")[0]   # filename.split("-") will give out ['10001','2023.1.18'] so add [0] to get just the 10001
+    invoice_no, date = filename.split("-")   # filename.split("-") will give out ['10001','2023.1.18'] so add [0] to get just the 10001
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt= f"Invoice No.{invoice_no}")
+    pdf.cell(w=50, h=8, txt=f"Invoice No.{invoice_no}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date :- {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    # Add a header
+    title_of_cell = df.columns
+    title_of_cell = [item.replace("_", " ").title() for item in title_of_cell]
+    pdf.set_font(family="Times", size=10,style="B")
+    pdf.set_text_color(80, 80, 80)
+
+    pdf.cell(w=30, h=8, txt=title_of_cell[0], border=1)
+    pdf.cell(w=40, h=8, txt=title_of_cell[1], border=1)
+    pdf.cell(w=60, h=8, txt=title_of_cell[2], border=1)
+    pdf.cell(w=40, h=8, txt=title_of_cell[3], border=1)
+    pdf.cell(w=40, h=8, txt=title_of_cell[4], border=1, ln=1)
+
+    # Adding elements in the cell
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+
+        pdf.cell(w=30, h=10, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=40, h=10, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=60, h=10, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=40, h=10, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=40, h=10, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
