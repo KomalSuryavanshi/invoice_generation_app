a41e4c4e719c9da817adb866bcdd0d4ee53e1225
Komal <komalwork11@gmail.com>
2023-04-03 11:34:59 +0530

    Add header and total price of products.

--- main.py
+++ main.py
@@ -44,4 +44,21 @@ for filepath in filepaths:
         pdf.cell(w=40, h=10, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=40, h=10, txt=str(row["total_price"]), border=1, ln=1)
 
+    #  Adding sum of total products
+    total_sum = df["total_price"].sum()
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    # To set other columns empty and only add total at last column
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="Total ", border=1)
+    pdf.cell(w=40, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Adding total sum sentence
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=30, h=8, txt=f"The total price is : {total_sum}", ln=1)
+
     pdf.output(f"PDFs/{filename}.pdf")
